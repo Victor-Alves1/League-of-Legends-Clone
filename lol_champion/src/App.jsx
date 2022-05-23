dad328ed3938c3de2ab8bc0ffcbdba83513e8b9b
Victor Alves da Costa <victoralves741@gmail.com>
2022-05-23 17:38:09 -0300

    tentando colocar galeria de imagens

--- lol_champion/src/App.jsx
+++ lol_champion/src/App.jsx
@@ -1,6 +1,6 @@
 //function wrap() { var args = new Array(arguments.length); for (var i = 0; i < args.length; i++) { args[i] = arguments[i]; } return fn.apply(thisArg, args); }
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 const apiJson= "http://ddragon.leagueoflegends.com/cdn/12.9.1/data/en_US/champion.json"
 const caraCampeao= "http://ddragon.leagueoflegends.com/cdn/12.9.1/img/champion/"
 const arrayChampions= ['Aatrox', 'Ahri', 'Akali', 'Alistar', 'Amumu', 'Anivia', 'Annie', 'Aphelios', 'Ashe', 'Aurelion Sol', 'Azir', 'Bardo', 'Blitzcrank', 'Brand', 'Braum', 'Caitlyn', 'Camille', 'Cassiopeia', 'Cho’Gath', 'Corki', 'Darius', 'Diana', 'Dr. Mundo', 'Draven', 'Ekko', 'Elise', 'Evelynn', 'Ezreal', 'Fiddlesticks', 'Fiora', 'Fizz', 'Galio', 'Gangplank', 'Garen', 'Gnar', 'Gragas', 'Graves', 'Hecarim', 'Heimerdinger', 'Illaoi', 'Irelia', 'Ivern', 'Janna', 'Jarvan Iv', 'Jax', 'Jayce', 'Jhin', 'Jinx', 'Kai’Sa', 'Kalista', 'Karma', 'Karthus', 'Kassadin', 'Katarina', 'Kayle',  'Kayn', 'Kennen', 'Kha’Zix', 'Kindred', 'Kled', 'Kog’Maw', 'Leblanc', 'Lee Sin', 'Leona', 'Lissandra', 'Lucian', 'Lulu', 'Lux', 'Malphite', 'Malzahar', 'Maokai', 'Master Yi', 'Miss Fortune', 'Mordekaiser', 'Morgana', 'Nami', 'Nasus', 'Nautilus', 'Neeko', 'Nidalee', 'Nocturne', 'Nunu E Willump', 'Olaf', 'Orianna', 'Ornn', 'Pantheon', 'Poppy', 'Pyke', 'Qiyana', 'Quinn', 'Rakan', 'Rammus', 'Rek’Sai', 'Renekton', 'Rengar', 'Riven', 'Rumble', 'Ryze', 'Sejuani', 'Senna', 'Sett', 'Shaco', 'Shen', 'Shyvana', 'Singed', 'Sion', 'Sivir', 'Skarner', 'Sona', 'Soraka', 'Swain', 'Sylas', 'Syndra', 'Tahm Kench', 'Taliyah', 'Talon', 'Taric', 'Teemo', 'Thresh', 'Tristana', 'Trundle', 'Tryndamere', 'Twisted Fate', 'Twitch', 'Udyr', 'Urgot', 'Varus', 'Vayne', 'Veigar', 'Vel’Koz', 'Vi', 'Viktor', 'Vladimir', 'Volibear', 'Warwick', 'Wukong', 'Xayah', 'Xerath', 'Xin Zhao', 'Yasuo', 'Yorick', 'Yuumi', 'Zac', 'Zed', 'Ziggs', 'Zilean', 'Zoe', 'Zyra']
@@ -8,27 +8,42 @@ const arrayChampions2=[]
 
 function App() {
 
-    useEffect(()=>{
-      axios.get("http://ddragon.leagueoflegends.com/cdn/12.9.1/data/en_US/champion.json")
-    .then(response=>response.data.data)
-    .then(response=>{
-      console.log(response)
-      for(let propriedade in response){
-        arrayChampions2.push(propriedade)
-      } 
-      console.log(arrayChampions2)         
-    })
-    .catch(erro => console.error("ops! ocorreu um erro" + erro)) })
-      
- 
+  const [camp, setCamp]=useState([])
+  const [nomes, setNomes]=useState([])
+  // const [fotos, setFotos]=useState([])
+  let fotenhas=[]
   
-  
+  const basicFetch = async ()=>{
+    const req = await fetch ("http://ddragon.leagueoflegends.com/cdn/12.9.1/data/en_US/champion.json")
+    const dado = await req.json()
+    const dados = await dado.data
+    setCamp(JSON.stringify(dados))
+    setNomes(Object.keys(dados))
+    for(let i=0; i<=159; i++){
+    fotenhas.push(<img src={`http://ddragon.leagueoflegends.com/cdn/12.9.1/img/champion/${nomes[i]}.png`} alt="" />)}
 
+    // for(let i=0; i<=159; i++){
+    //   const newFoto = ()=> [ <img src={`http://ddragon.leagueoflegends.com/cdn/12.9.1/img/champion/${nomes[i]}.png`} alt="" />]
+    //   setFotos(newFoto) 
+    // }
+    
+    // console.log(fotos)
+    
+    return dados
+  } 
+
+    useEffect(()=>{
+      basicFetch().catch(e=>console.log(e))
+    },[])
+   
+  
+    console.log(fotenhas)
+    
   return (
     <div className="App">
-      <p id="teste">
-        Campeão:
-      </p>
+      
+      {/* {fotos} */}
+      {camp}
     </div>
   )
 }
