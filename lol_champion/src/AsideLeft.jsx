f0d8a1fa3b92a42f5fbf1ca7e3cc33a3f511b9ff
Victor Alves da Costa <victoralves741@gmail.com>
2022-05-26 09:20:24 -0300

    left aside upgrade

--- lol_champion/src/AsideLeft.jsx
+++ lol_champion/src/AsideLeft.jsx
@@ -2,7 +2,23 @@ import "./AsideLeft.css"
 const AsideLeft = () => {
     return (
         <div className="containerLeft">
-            ppppppp
+            <div className="mastery-numbers">
+                <div className="total-score-mastery">
+                    <h2>100</h2>
+                    <p>pontuação total de maestria</p>
+                </div>
+                <div className="goals-reached-mastery">
+                    <h2>100</h2>
+                    <p>marcos alcançados</p>
+                </div>
+            </div>
+            <input type="text" name="" id="" placeholder="Buscar"/>
+            <select name="" id="">
+                <option value="">Propriedade</option>
+            </select>
+            <select name="" id="">
+                <option value="">Marcos alcançados</option>
+            </select>
         </div>
     );
 }
